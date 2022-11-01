67016f8e120f049af1426284dc6af6549c1d3a7f
Viruzzz <nahuelsinka@gmail.com>
2022-11-01 19:06:50 -0300

    comand timer updated

--- cmdHandler/test.js
+++ cmdHandler/test.js
@@ -9,11 +9,13 @@ module.exports = {
     const secToMilg = (tim) => tim * 1000;
     const minToMilg = (tim) => tim * 60000;
     const hrToMilg = (tim) => tim * 3600000;
+    const dayToMilg = (tim) => tim * 86400000;
 
     const formatSeg = (ms) => (ms / 1000) % 60;
     const formatMin = (ms) => (ms / 60000) % 60;
     const formatHr = (ms) => (ms / 3600000) % 24;
-    
+    const formatDias = (ms) => ms / 86400000;
+
     let mili;
     let unidadString;
 
@@ -32,6 +34,9 @@ module.exports = {
     } else if (unidad == "h") {
       mili = hrToMilg(time);
       unidadString = "horas";
+    } else if (unidad == "dias" || unidad == "dia") {
+      mili = dayToMilg(time);
+      unidadString = "dias";
     }
 
     // Indica el tiempo puesto a temporizar
@@ -39,32 +44,39 @@ module.exports = {
 
     const div = (75 * mili) / 100;
     setTimeout(async () => {
-      const elemet0 = unidad == "s" && time < 60;
+      const elemet0 = unidad == "s" && time < 240;
       const elemet1 = unidad == "m" && time < 4;
-      const elemet2 = unidad == "m" && time < 60;
+
+      const elemet2 = unidad == "m" && time < 240;
       const elemet3 = unidad == "h" && time < 4;
-      const elemet4 = unidad == "h" && time < 24;
+
+      const elemet4 = unidad == "h" && time < 96;
       let rest;
 
+      // rest = mili - div;
       if (elemet0 || elemet1) {
         rest = formatSeg(mili - div);
         unidadString = "seg";
-        // console.log("elemet", mili - div);
+        console.log("seg", mili - div);
       } else if (elemet2 || elemet3) {
         rest = formatMin(mili - div);
         unidadString = "min";
-        // console.log("elemet2", mili - div);
+        console.log("min", mili - div);
       } else if (elemet4) {
         rest = formatHr(mili - div);
         unidadString = "horas";
-        // console.log("elemet4", mili - div);
+        console.log("hr", mili - div);
+      } else {
+        rest = formatDias(mili - div).toFixed(2);
+        unidadString = "dias";
+        console.log("dias", mili - div);
       }
 
       const mgs = await message.channel.send(
         `Queda **${rest} ${unidadString}**`
       );
       // mgs.edit('a')
-    }, mili);
+    }, 1000);
 
     setTimeout(async () => {
       const mgs = await message.channel.send(`BOOM!! ${client.user}`);
