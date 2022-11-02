e1b2a13735c92bcc342378d9b5dfac2ccc0c01df
Viruzzz <nahuelsinka@gmail.com>
2022-11-02 00:11:29 -0300

    convertTime en testeo

--- cmdHandler/test.js
+++ cmdHandler/test.js
@@ -1,86 +1,22 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "test",
   alias: ["t"],
 
   async execute(client, message, args) {
-    const arg = args.join("").split("");
-    const unidad = arg.pop();
-    const time = arg.join("");
-    const secToMilg = (tim) => tim * 1000;
-    const minToMilg = (tim) => tim * 60000;
-    const hrToMilg = (tim) => tim * 3600000;
-    const dayToMilg = (tim) => tim * 86400000;
+    const embed = new EmbedBuilder()
+      .setTitle(`Convert`)
+      .setColor("blue")
+      .setFields([
+        { name: "minutos   =", value: "5000", inline: true },
+        { name: "segundo", value: "5", inline: true }
+      ])
+      // .setFooter({
+      //   text: "Usar el prefix -",
+      // })
+      // .setTimestamp();
 
-    const formatSeg = (ms) => (ms / 1000) % 60;
-    const formatMin = (ms) => (ms / 60000) % 60;
-    const formatHr = (ms) => (ms / 3600000) % 24;
-    const formatDias = (ms) => ms / 86400000;
-
-    let mili;
-    let unidadString;
-
-    if (!args) return message.channel.send("Escribe los segundos");
-    if (!unidad == "m" || !unidad == "s" || !unidad == "h")
-      return message.channel.send("Escribe la unidad");
-
-    // Transforma el tiempo puesto por el user y lo pasa a milisegundos
-    // Elige la unidad para devolverlo en string
-    if (unidad == "s") {
-      mili = secToMilg(time);
-      unidadString = "seg";
-    } else if (unidad == "m") {
-      mili = minToMilg(time);
-      unidadString = "min";
-    } else if (unidad == "h") {
-      mili = hrToMilg(time);
-      unidadString = "horas";
-    } else if (unidad == "dias" || unidad == "dia") {
-      mili = dayToMilg(time);
-      unidadString = "dias";
-    }
-
-    // Indica el tiempo puesto a temporizar
-    message.channel.send(`Timer: **${time} ${unidadString}**`);
-
-    const div = (75 * mili) / 100;
-    setTimeout(async () => {
-      const elemet0 = unidad == "s" && time < 240;
-      const elemet1 = unidad == "m" && time < 4;
-
-      const elemet2 = unidad == "m" && time < 240;
-      const elemet3 = unidad == "h" && time < 4;
-
-      const elemet4 = unidad == "h" && time < 96;
-      let rest;
-
-      // rest = mili - div;
-      if (elemet0 || elemet1) {
-        rest = formatSeg(mili - div);
-        unidadString = "seg";
-        console.log("seg", mili - div);
-      } else if (elemet2 || elemet3) {
-        rest = formatMin(mili - div);
-        unidadString = "min";
-        console.log("min", mili - div);
-      } else if (elemet4) {
-        rest = formatHr(mili - div);
-        unidadString = "horas";
-        console.log("hr", mili - div);
-      } else {
-        rest = formatDias(mili - div).toFixed(2);
-        unidadString = "dias";
-        console.log("dias", mili - div);
-      }
-
-      const mgs = await message.channel.send(
-        `Queda **${rest} ${unidadString}**`
-      );
-      // mgs.edit('a')
-    }, 1000);
-
-    setTimeout(async () => {
-      const mgs = await message.channel.send(`BOOM!! ${client.user}`);
-      // mgs.edit('a')
-    }, mili);
+    message.channel.send({ embeds: [embed] });
   },
 };
--- slashcmd/testSlash.js
+++ slashcmd/testSlash.js
@@ -1,5 +1,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const axios = require("axios");
+const { EmbedBuilder } = require("discord.js");
+
+const mili = { name: "Milisegundo", value: "millisecond" };
+const seg = { name: "Segundo", value: "second" };
+const min = { name: "Minutos", value: "minutes" };
+const hora = { name: "Horas", value: "hours" };
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,36 +12,103 @@ module.exports = {
     .setDescription("#############")
     .addStringOption((option) =>
       option
-        .setName("text")
-        .setDescription("Inserte el texto")
+        .setName("time")
+        .setDescription("Elige el formato de tu numero")
         .setRequired(true)
+        .addChoices(mili, seg, min, hora)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("numero")
+        .setDescription("Ingrese un numero")
+        .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("to")
+        .setDescription("Elige a que formato quiere convertirlo")
+        .setRequired(true)
+        .addChoices(mili, seg, min, hora)
     ),
 
   async run(client, interaction) {
-    const texto = interaction.options.getString("text");
-    await interaction.deferReply();
+    const time = interaction.options.getString("time");
+    const num = interaction.options.getNumber("numero");
+    const toTime = interaction.options.getString("to");
+    let numReturn;
 
-    const encodedParams = new URLSearchParams();
-    encodedParams.append("q", texto);
-    encodedParams.append("target", "en");
-    encodedParams.append("source", "es");
+    const milgToSec = (tim) => tim / 1000;
+    const milgToMin = (tim) => tim / 60000;
+    const milgToHr = (tim) => tim / 3600000;
+    const milgToDay = (tim) => tim / 86400000;
 
-    const options = {
-      method: "POST",
-      url: "https://google-translate1.p.rapidapi.com/language/translate/v2",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "Accept-Encoding": "application/gzip",
-        "X-RapidAPI-Key": "53351dee31msh305061050cf7042p17eaedjsnc91dc9d740c5",
-        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
-      },
-      data: encodedParams,
-    };
+    const secToMilg = (tim) => tim * 1000;
+    const secToMin = (tim) => tim / 60;
+    const secToHr = (tim) => tim / 3600;
+    const secToDay = (tim) => tim / 86400;
 
-    const response = await axios(options);
-    const res = response.data.data.translations[0].translatedText;
-    await interaction.editReply({
-      content: `${res}`,
+    const minToMilg = (tim) => tim * 60000;
+    const minToSec = (tim) => tim * 60;
+    const minToHr = (tim) => tim / 60;
+    const minToDay = (tim) => tim / 1440;
+
+    const hrToMilg = (tim) => tim * 3600000;
+    const hrToSec = (tim) => tim * 3600;
+    const hrToMin = (tim) => tim * 60;
+    const hrToDay = (tim) => tim / 24;
+
+    const dayToMilg = (tim) => tim * 86400000;
+    const dayToSec = (tim) => tim * 86400;
+    const dayToMin = (tim) => tim * 1440;
+    const dayToHr = (tim) => tim * 24;
+
+    console.log(time);
+    console.log(toTime);
+
+    if (time == "millisecond") {
+      if (toTime == "second") numReturn = milgToSec(num);
+      else if (toTime == "minutes") numReturn = milgToMin(num);
+      else if (toTime == "hours") numReturn = milgToHr(num);
+      else numReturn = num
+    }
+    else if (time == "second") {
+      if (toTime == "millisecond") numReturn = secToMilg(num);
+      else if (toTime == "minutes") numReturn = secToMin(num);
+      else if (toTime == "hours") numReturn = secToHr(num);
+      else numReturn = num
+    }
+    else if (time == "minutes") {
+      if (toTime == "millisecond") numReturn = minToMilg(num);
+      else if (toTime == "second") numReturn = minToSec(num);
+      else if (toTime == "hours") numReturn = minToHr(num);
+      else numReturn = num
+    }
+    else if (time == "hours") {
+      if (toTime == "millisecond") numReturn = hrToMilg(num);
+      else if (toTime == "second") numReturn = hrToSec(num);
+      else if (toTime == "minutes") numReturn = hrToMin(num);
+      else numReturn = num
+    }
+
+    console.log(numReturn);
+
+    const embed = new EmbedBuilder()
+      .setTitle(`Convert`)
+      .setColor("blue")
+      .setFields([
+        { name: `${time}`, value: `${num}`, inline: true },
+        { name: `${toTime}`, value: `${numReturn}`, inline: true }
+      ])
+      // .setFields([
+      //   { name: `${time}`, value: `${num}`},
+      //   { name: `${toTime}`, value: `${numReturn}`}
+      // ])
+      // .setFooter({
+      //   text: "valor dividido entre 60",
+      // });
+      
+    await interaction.reply({
+      embeds: [embed],
     });
   },
 };
